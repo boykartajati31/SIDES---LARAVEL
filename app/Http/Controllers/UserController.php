8808690598fd8defc3eb0bf548616da6461f1104
boykartajati31 <boykertajati1998@gmail.com>
2025-08-25 11:21:17 +0700

    mengaitkan akun penduduk dan user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Resident;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,20 +16,35 @@ class UserController extends Controller
     {
         // Mengambil semua user dengan status 'submitted'
         $users = User::where('status', 'submitted')->get();
+        $residents = Resident::where('user_id', null)->get();
 
         // Mengirim data user ke view
         return view('pages.account-request.index', [
-            'users' => $users
+            'users' => $users,
+            'residents' => $residents,
         ]);
     }
 
     public function account_approval(Request $request, $userId)
     {
+        $request->validate([
+            'for' => ['required', Rule::in(['approve', 'reject', 'activate', 'deactivate'])],
+            'resident_id' => ['nullable', 'exists:residents,id']
+        ]);
+
         $for = $request->input('for');
 
         $user = User::findOrFail($userId);
         $user->status = ($for == 'approve' || $for == 'activate') ? 'approved' : 'rejected';
         $user->save();
+
+        $resident_id = $request->input('resident_id');
+
+        if ($request->has('resident_id') && isset ($resident_id)) {
+            Resident::where('id', $resident_id)->update([
+                'user_id' => $user->id,
+            ]);
+        }
 
         if($for == 'activate') {
             return back()->with('success','Success Activate account');
